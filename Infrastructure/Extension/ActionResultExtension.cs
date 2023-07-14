5f85529137117ec26c6903e0280b1a8296f8818c
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-07-14 17:08:17 +0100

    Extension to convert custom action result to mvc action result

---
+++ Infrastructure/Extension/ActionResultExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace AdeNote.Infrastructure.Extension
+{
+    public static class ActionResultExtension
+    {
+        public static IActionResult Response(this TasksLibrary.Utilities.ActionResult actionResult)
+        {
+            return actionResult.StatusCode switch
+            {
+                (int)HttpStatusCode.OK => new OkObjectResult(actionResult),
+                (int)HttpStatusCode.BadRequest => new BadRequestObjectResult(actionResult),
+                (int)HttpStatusCode.Unauthorized => new UnauthorizedObjectResult(actionResult),
+                (int)HttpStatusCode.InternalServerError => new ObjectResult(actionResult),
+                (int)HttpStatusCode.NotFound => new NotFoundObjectResult(actionResult),
+            };
+        }
+    }
+}
