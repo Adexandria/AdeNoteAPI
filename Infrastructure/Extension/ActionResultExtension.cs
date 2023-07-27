a2b8202896c91e096d16b9f9db32ce1190e7d891
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-07-27 10:20:01 +0100

    Status code for internal server error can be displayed

--- Infrastructure/Extension/ActionResultExtension.cs
+++ Infrastructure/Extension/ActionResultExtension.cs
@@ -7,7 +7,7 @@ namespace AdeNote.Infrastructure.Extension
     {
         public static IActionResult Response(this TasksLibrary.Utilities.ActionResult actionResult)
         {
-            return actionResult.StatusCode switch
+            var result = actionResult.StatusCode switch
             {
                 (int)HttpStatusCode.OK => new OkObjectResult(actionResult),
                 (int)HttpStatusCode.BadRequest => new BadRequestObjectResult(actionResult),
@@ -15,6 +15,11 @@ namespace AdeNote.Infrastructure.Extension
                 (int)HttpStatusCode.InternalServerError => new ObjectResult(actionResult),
                 (int)HttpStatusCode.NotFound => new NotFoundObjectResult(actionResult),
             };
+
+            if (actionResult.StatusCode == (int)HttpStatusCode.InternalServerError)
+                result.StatusCode = 500;
+
+            return result;
         }
     }
 }
