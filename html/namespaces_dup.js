8fba60bba29102253aa0095ede83bc777101576f
Adeola-Aderibigbe <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-07-28 12:04:56 +0000

    Deploying to gh-pages from @ Adeola-Aderibigbe/AdeNoteAPI@d4c52ded48c273064950ad2d1d4e1197590dc9b6 🚀

--- html/namespaces_dup.js
+++ html/namespaces_dup.js
@@ -6,5 +6,6 @@ var namespaces_dup =
       [ "Db", "md_README.html#autotoc_md4", null ],
       [ "Models", "md_README.html#autotoc_md5", null ]
     ] ],
+    [ "<a href=\"https://adenoteapi.azurewebsites.net/swagger/index.html\">Swagger Documentation</a>", "md_README.html#autotoc_md6", null ],
     [ "AdeNote", "namespaceAdeNote.html", "namespaceAdeNote" ]
 ];
