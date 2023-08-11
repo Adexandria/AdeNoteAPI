1c8b19cb38e09cfdc448b91ecb80e0008c644be0
Adeola-Aderibigbe <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-08-11 08:39:47 +0000

    Deploying to gh-pages from @ Adeola-Aderibigbe/AdeNoteAPI@04286648cb5973ae54507dd61bfe0fe2fc1939bd 🚀

---
+++ html/classAdeNote_1_1Controllers_1_1NoteController.js
@@ -0,0 +1,9 @@
+var classAdeNote_1_1Controllers_1_1NoteController =
+[
+    [ "NoteController", "classAdeNote_1_1Controllers_1_1NoteController.html#aa352a9933e35f62730e3c8468f0e4c1a", null ],
+    [ "CreateNote", "classAdeNote_1_1Controllers_1_1NoteController.html#ac92ff7f55a93a4704b8cb44cbe049284", null ],
+    [ "DeleteNote", "classAdeNote_1_1Controllers_1_1NoteController.html#a080f21a0c5031ff2563a0a6e9453a33f", null ],
+    [ "GetAllNotes", "classAdeNote_1_1Controllers_1_1NoteController.html#ada181a129615c54c048b598aaf19e1b0", null ],
+    [ "GetNote", "classAdeNote_1_1Controllers_1_1NoteController.html#a37cc8483bcdc0693efea6f22daa05355", null ],
+    [ "UpdateNote", "classAdeNote_1_1Controllers_1_1NoteController.html#ae864f319adb1cb28e82aadf22fdad3ff", null ]
+];
---
+++ html/dir_4a45a580241f65091c8946339a3eee2e.js
@@ -0,0 +1,6 @@
+var dir_4a45a580241f65091c8946339a3eee2e =
+[
+    [ "NoteDbContext.cs", "NoteDbContext_8cs.html", [
+      [ "NoteDbContext", "classAdeNote_1_1Db_1_1NoteDbContext.html", "classAdeNote_1_1Db_1_1NoteDbContext" ]
+    ] ]
+];
