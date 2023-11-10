8fdcfa87d9b57f5fe0c6e38a38615458b949935a
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-11-10 15:30:26 +0100

    created tables and fix bug issues

--- AdeNote.API/Infrastructure/Utilities/Database.cs
+++ AdeNote.API/Infrastructure/Utilities/Database.cs
@@ -1,14 +1,23 @@
 ﻿using AdeNote.Db;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using TasksLibrary.Architecture.Application;
 
 namespace AdeNote.Infrastructure.Utilities
 {
     public static class Database
     {
-        public static void CreateTables(this IServiceProvider provider)
+        public static void CreateTables(this IServiceProvider provider, TaskContainerBuilder containerBuilder)
         {
             var dbContext = provider.GetService<NoteDbContext>() ?? throw new NullReferenceException("Unregistered service");
 
-            dbContext.Database.EnsureCreated();
+            var databaseCreator = dbContext.GetService<IRelationalDatabaseCreator>();
+
+            if (databaseCreator.HasTables())
+            {
+                containerBuilder.BuildMigration();
+                databaseCreator.CreateTables();
+            }
         }
     }
 }
