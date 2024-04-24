01a8b293ba4d7530e1192a662499a3b3b07bac41
Adeola-Aderibigbe <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2024-04-24 08:38:55 +0000

    Deploying to gh-pages from @ Adexandria/AdeNoteAPI@53573fa8efd63452b1bded71c9cd419da9111ef4 🚀

--- html/classAdeNote_1_1Controllers_1_1BookController.js
+++ html/classAdeNote_1_1Controllers_1_1BookController.js
@@ -3,7 +3,7 @@ var classAdeNote_1_1Controllers_1_1BookController =
     [ "BookController", "classAdeNote_1_1Controllers_1_1BookController.html#a86eacd2352b5ae21807babe70693f9e6", null ],
     [ "CreateBook", "classAdeNote_1_1Controllers_1_1BookController.html#a9cf66d0add6be7346f03e4f5abdb91a6", null ],
     [ "DeleteBook", "classAdeNote_1_1Controllers_1_1BookController.html#a346c0ed0eb0403993ca48c0b18406547", null ],
-    [ "ExportBooks", "classAdeNote_1_1Controllers_1_1BookController.html#a6b969dc1be8698fe8fe55254233e244c", null ],
+    [ "ExportBooks", "classAdeNote_1_1Controllers_1_1BookController.html#a24bf39b6873e8d4c3feefdb3a091784b", null ],
     [ "GetAllBooks", "classAdeNote_1_1Controllers_1_1BookController.html#ad906923347f5e4793921d7fbb7d96cf1", null ],
     [ "GetBook", "classAdeNote_1_1Controllers_1_1BookController.html#a84ebe72dc37af1f6c2ac1e2380a8b99d", null ],
     [ "ImportBooks", "classAdeNote_1_1Controllers_1_1BookController.html#a705305b7b6aff9184dc497815ae901ad", null ],
