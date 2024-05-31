4592a99c1fb86cf5b62b9c23d8c8574b3433b2e6
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-05-31 14:09:28 +0100

    update unit tests

--- AdeNote.API/Infrastructure/Services/BookService.cs
+++ AdeNote.API/Infrastructure/Services/BookService.cs
@@ -174,10 +174,11 @@ namespace AdeNote.Infrastructure.Services
 
                 var currentBook = await bookRepository.GetAsync(bookId,userId);
 
-                currentBook.SetModifiedDate();
-
                 if (currentBook == null)
                     return ActionResult.Failed("Book does not exist", (int)HttpStatusCode.NotFound);
+
+                currentBook.SetModifiedDate();
+
 
                 var commitStatus = await bookRepository.Update(book);
                 if (!commitStatus)
