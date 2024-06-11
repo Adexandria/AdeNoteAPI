bb47d8778ca078ee6dbf3f348631fd6343bcce4c
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-06-11 12:26:52 +0100

    Add health checks

---
+++ AdeNote.API/Infrastructure/Utilities/HealthChecks/DatabaseHealthCheck.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace AdeNote.Infrastructure.Utilities.HealthChecks
+{
+    public class DatabaseHealthCheck : IHealthCheck
+    {
+        public DatabaseHealthCheck(IConfiguration config)
+        {
+            connectionString = config.GetConnectionString("NotesDB");
+        }
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand("SELECT 1", connection))
+            {
+                try
+                {
+                    connection.Open();
+                    var result = await command.ExecuteScalarAsync(cancellationToken);
+
+                    if (result != null && result.Equals(1))
+                    {
+                        return HealthCheckResult.Healthy();
+                    }
+                    else
+                    {
+                        return HealthCheckResult.Unhealthy("Database query did not return 1.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return HealthCheckResult.Unhealthy(ex.Message);
+                }
+            }
+        }
+
+        private readonly string connectionString;
+    }
+}
