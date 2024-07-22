4018285fcb2542de3498c5cbc84025fc367b0bcd
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-07-22 10:56:04 +0100

    Create azure ad settings

---
+++ AdeNote.API/Infrastructure/Utilities/SSO/AzureAdConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace AdeNote.Infrastructure.Utilities.SSO
+{
+    public class AzureAdConfiguration
+    {
+        public string Audience { get; set; }
+        public string Instance { get; set; }
+        public string TenantId { get; set; }
+    }
+}
