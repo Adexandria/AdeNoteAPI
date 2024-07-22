b16ebe21ada3c607f9338348511dd1c3b27a5fa2
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-07-22 10:51:58 +0100

    Creates queue and exchange

---
+++ AdeNote.API/Infrastructure/Extension/EventExtension.cs
@@ -0,0 +1,28 @@
+﻿using AdeMessaging.Services;
+using AdeNote.Infrastructure.Utilities.EventSystem;
+
+namespace AdeNote.Infrastructure.Extension
+{
+    public static class EventExtension
+    {
+
+        public static void SetUpRabbitConfiguration(this WebApplication app, IConfiguration config)
+        {
+            using var scope = app.Services.CreateScope();
+            var serviceProvider = scope.ServiceProvider;
+
+            var messagingEvent = serviceProvider.GetRequiredService<IMessagingService>();
+
+            var eventConfiguration = config.GetSection("Events")
+                .GetSection("Ticket")
+                .Get<EventConfiguration>();
+
+
+            messagingEvent.CreateExchange(eventConfiguration.Exchange);
+
+            messagingEvent.CreateQueue(eventConfiguration.Queue, eventConfiguration.Exchange, eventConfiguration.RoutingKey);
+
+            
+        }
+    }
+}
