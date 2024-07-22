c1898e800cf546a537ca5c48fa1fcf68ee4f7c4d
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-07-22 10:33:49 +0100

    Add extension to extract description attribute from enum

---
+++ AdeMessaging/Services/Extensions/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace AdeMessaging.Services.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum source) 
+        {
+            var enumField = source.GetType().GetField(source.ToString());
+            if(enumField == null)
+                return string.Empty;
+           
+            var descriptionAttributes = (DescriptionAttribute[])enumField.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if(descriptionAttributes.Length > 0)
+                return descriptionAttributes[0].Description;
+           
+            return source.ToString();
+        }
+
+    }
+}
