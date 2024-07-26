856e2f1b1da044c7e1b94f3b4048801ba1104c01
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-07-26 15:56:48 +0100

    Set up identity context that includes roles and users

---
+++ AdeAuth/Db/IdentityContext.cs
@@ -0,0 +1,37 @@
+﻿using AdeAuth.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AdeAuth.Db
+{
+
+    public class IdentityContext : DbContext 
+    {
+        public IdentityContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<ApplicationRole> Roles { get; set; }
+
+        public DbSet<ApplicationUser> Users { get; set; }
+    }
+
+    public class IdentityContext<TModel> : IdentityContext where TModel : ApplicationUser
+    {
+        public IdentityContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+
+    public class IdentityContext<TUser,TRole> : IdentityContext 
+        where TUser : ApplicationUser
+        where TRole : ApplicationRole
+    {
+        public IdentityContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
