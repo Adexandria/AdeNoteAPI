1665276e67647ab205f3e3759c2364d40399fb8e
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-07-26 15:54:25 +0100

    Create internal model to handle transactions when there is no custom

---
+++ AdeAuth/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdeAuth.Models
+{
+    internal class User : ApplicationUser
+    {
+        public User():base()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public new Guid Id { get; set; }
+        public new string FirstName { get; set; }
+        public new string LastName { get; set; }
+        public new string UserName { get; set; }
+        public new string Email { get; set; }
+        public new bool EmailConfirmed { get; set; }
+        public new string Salt { get; set; }
+        public new string PasswordHash { get; set ; }
+        public new string PhoneNumber { get; set ; }
+        public new bool PhoneNumberConfirmed { get; set; }
+        public new bool TwoFactorEnabled { get; set; }
+        public new int TwoFactorType { get; set; }
+        public new string AuthenticatorKey { get; set; }
+        public new bool LockoutEnabled { get; set; }
+    }
+}
