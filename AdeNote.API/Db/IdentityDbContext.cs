e5636e753615a247a77ad06922960ff85b1a8890
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-07-26 15:59:37 +0100

    Set up identity db context

---
+++ AdeNote.API/Db/IdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AdeAuth.Db;
+using AdeNote.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdeNote.Db
+{
+    public class IdentityDbContext: IdentityContext<User>
+    {
+        public IdentityDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
