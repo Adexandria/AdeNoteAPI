f0e92d72c626f5c9feab5326de1f5273cf631df2
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-07-30 16:29:29 +0100

    Add dbcontext to manage models

--- AdeAuth/Db/IdentityContext.cs
+++ AdeAuth/Db/IdentityContext.cs
@@ -12,26 +12,72 @@ namespace AdeAuth.Db
 
         }
 
-        public DbSet<ApplicationRole> Roles { get; set; }
+        public  DbSet<ApplicationRole> Roles { get; set; }
 
-        public DbSet<ApplicationUser> Users { get; set; }
-    }
+        public  DbSet<ApplicationUser> Users { get; set; }
 
-    public class IdentityContext<TModel> : IdentityContext where TModel : ApplicationUser
-    {
-        public IdentityContext(DbContextOptions options) : base(options)
+        public  DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationRole>()
+                .HasMany<ApplicationUser>()
+                .WithMany()
+                .UsingEntity<UserRole>
+                (l=> l.HasOne<ApplicationUser>().WithMany().HasForeignKey("UserId"),
+                 r => r.HasOne<ApplicationRole>().WithMany().HasForeignKey("RoleId"));
         }
     }
 
-    public class IdentityContext<TUser,TRole> : IdentityContext 
+    public class IdentityContext<TUser> : DbContext where TUser : ApplicationUser
+    {
+        public IdentityContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+        public  DbSet<TUser> Users { get; set; }
+
+        public  DbSet<ApplicationRole> Roles { get; set; }
+
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationRole>()
+                .HasMany<TUser>()
+                .WithMany()
+                .UsingEntity<UserRole>(l => l.HasOne<TUser>().WithMany().HasForeignKey("UserId"),
+                 r => r.HasOne<ApplicationRole>().WithMany().HasForeignKey("RoleId"));
+        }
+    }
+
+    public class IdentityContext<TUser,TRole> : DbContext
         where TUser : ApplicationUser
         where TRole : ApplicationRole
     {
-        public IdentityContext(DbContextOptions options) : base(options)
+        public IdentityContext(DbContextOptions options):base(options)
         {
 
+        }
+        public  DbSet<TUser> Users { get; set; }
+        public  DbSet<TRole> Roles { get; set; }
+
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TRole>()
+                .HasMany<TUser>()
+                .WithMany()
+                .UsingEntity<UserRole>(l => l.HasOne<TUser>().WithMany().HasForeignKey("UserId"),
+                 r => r.HasOne<TRole>().WithMany().HasForeignKey("RoleId"));
         }
     }
 }
