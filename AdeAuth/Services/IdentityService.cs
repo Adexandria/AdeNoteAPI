80841267121133b1d1bc56a3180224312e0010d2
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-07-30 16:27:35 +0100

    Add abstract functionalities to make it overrideable

---
+++ AdeAuth/Services/IdentityService.cs
@@ -0,0 +1,14 @@
+﻿using AdeAuth.Models;
+using AdeAuth.Services.Interfaces;
+
+
+namespace AdeAuth.Services
+{
+    public abstract class IdentityService<TModel> : IUserService<TModel>
+        where TModel : ApplicationUser
+    {
+        public abstract Task<TModel> AuthenticateUsingEmailAsync(string email, string password);
+        public abstract Task<TModel> AuthenticateUsingUsernameAsync(string username, string password);
+        public abstract Task<bool> CreateUserAsync(TModel user);
+    }
+}
---
+++ AdeAuth/Services/RoleManager.cs
@@ -0,0 +1,17 @@
+﻿using AdeAuth.Models;
+using AdeAuth.Services.Interfaces;
+
+
+namespace AdeAuth.Services
+{
+    public abstract class RoleManager<TRole> : IRoleService<TRole>
+        where TRole : ApplicationRole
+    {
+        public abstract Task<bool> AddUserRole(Guid userId, string roleName);
+        public abstract Task<bool> CreateRoleAsync(TRole role);
+        public abstract Task<bool> CreateRolesAsync(List<TRole> roles);
+        public abstract Task<bool> DeleteRoleAsync(string role);
+        public abstract Task<bool> DeleteRolesAsync(string[] roles);
+        public abstract Task<bool> RemoveUserRole(Guid userId, string roleName);
+    }
+}
