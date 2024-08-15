72da67e58da81421d53a9a93d16c508302c3aba2
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-08-15 15:51:45 +0100

    Handles property information

---
+++ AdeCache/Services/Utilities/PropertyInformation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdeCache.Services.Utilities
+{
+    /// <summary>
+    /// Manages property information
+    /// </summary>
+    internal class PropertyInformation
+    {
+        /// <summary>
+        /// Name of the property
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Type of the property
+        /// </summary>
+        public Type PropertyType { get; set; }
+    }
+}
