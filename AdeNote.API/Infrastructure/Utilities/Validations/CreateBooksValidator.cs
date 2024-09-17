54447d42fe4db0dd29cb02f5f4374722f387e732
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-09-17 15:35:58 +0100

    Add validators for books

---
+++ AdeNote.API/Infrastructure/Utilities/Validations/CreateBooksValidator.cs
@@ -0,0 +1,31 @@
+﻿using AdeNote.Infrastructure.Requests.CreateBooks;
+using FluentValidation;
+
+namespace AdeNote.Infrastructure.Utilities.Validations
+{
+    public class CreateBooksValidator: AbstractValidator<CreateBooksRequest>
+    {
+        public CreateBooksValidator()
+        {
+            RuleFor(s => s.UserId).NotNull().WithMessage("Invalid user id");
+
+            RuleFor(s => s.CreateBooks).NotNull().WithMessage("Request body cannot be null");
+
+            RuleForEach(s => s.CreateBooks)
+                .Custom((list, context) =>
+                {
+                    var isEmpty =  string.IsNullOrEmpty(list.Title);
+                    if (isEmpty)
+                        context.AddFailure("Invalid title");
+                });
+
+            RuleForEach(s => s.CreateBooks)
+               .Custom((list, context) =>
+               {
+                   var isEmpty = string.IsNullOrEmpty(list.Description);
+                   if (isEmpty)
+                       context.AddFailure("Invalid description");
+               });
+        }
+    }
+}
---
+++ AdeNote.API/Infrastructure/Utilities/Validations/UpdateBookValidator.cs
@@ -0,0 +1,31 @@
+﻿using AdeNote.Infrastructure.Requests.UpdateBook;
+using FluentValidation;
+
+namespace AdeNote.Infrastructure.Utilities.Validations
+{
+    public class UpdateBookValidator: AbstractValidator<UpdateBookRequest>
+    {
+        public UpdateBookValidator()
+        {
+            RuleFor(s => s.UserId).NotEmpty().WithMessage("Invalid user id");
+
+            RuleFor(s => s.BookId).NotEmpty().WithMessage("Invalid book id");
+
+            RuleFor(s => s.UpdateBook)
+             .Custom((list, context) =>
+             {
+                 var isEmpty = string.IsNullOrEmpty(list.Title);
+                 if (isEmpty)
+                     context.AddFailure("Invalid title");
+             });
+
+            RuleFor(s => s.UpdateBook)
+               .Custom((list, context) =>
+               {
+                   var isEmpty = string.IsNullOrEmpty(list.Description);
+                   if (isEmpty)
+                       context.AddFailure("Invalid description");
+               });
+        }
+    }
+}
