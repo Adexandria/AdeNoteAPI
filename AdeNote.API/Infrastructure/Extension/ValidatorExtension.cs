7bbcdf9f6bb7cd8df585d8b0721d14f96cd3d8e8
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-09-19 16:28:26 +0100

    Handles validation for ticket status

---
+++ AdeNote.API/Infrastructure/Extension/ValidatorExtension.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace AdeNote.Infrastructure.Extension
+{
+    public static class ValidatorExtension
+    {
+        public static IRuleBuilderOptions<T,string> IsValidTicketStatus<T> (this IRuleBuilder<T,string> ruleBuilder)
+        {
+            return ruleBuilder.Must(x =>
+             {
+                 var isValidStatus = new[] { "Pending", "Inreview", "Resolved", "Unresolved" };
+                 if (isValidStatus.Contains(x))
+                 {
+                     return true;
+                 }
+                 return false;
+             });
+        }
+    }
+}
