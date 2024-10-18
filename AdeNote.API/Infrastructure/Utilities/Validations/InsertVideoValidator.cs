a1c5798540733e89a0677245f74064ca5dd417c8
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-10-18 14:14:23 +0100

    validate stream length

--- AdeNote.API/Infrastructure/Utilities/Validations/InsertVideoValidator.cs
+++ AdeNote.API/Infrastructure/Utilities/Validations/InsertVideoValidator.cs
@@ -9,7 +9,13 @@ namespace AdeNote.Infrastructure.Utilities.Validations
         {
             RuleFor(s => s.BookId).NotEmpty().WithMessage("Invalid book id");
             RuleFor(s => s.PageId).NotEmpty().WithMessage("Invalid page id");
-            RuleFor(s => s.Description).NotEmpty().WithMessage("Invalid description");
+            RuleFor(s => s.Stream).Custom((s, context) =>
+            {
+                if(s.Length <= 0) 
+                {
+                    context.AddFailure("Invalid video content");
+                }
+            });
         }
     }
 }
