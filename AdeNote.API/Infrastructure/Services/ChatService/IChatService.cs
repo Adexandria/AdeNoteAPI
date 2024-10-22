0767a58710779f09a0fe025402992ad1ce17ff42
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-10-22 16:00:28 +0100

    Service to manage threads

--- AdeNote.API/Infrastructure/Services/ChatService/IChatService.cs
+++ AdeNote.API/Infrastructure/Services/ChatService/IChatService.cs
@@ -1,4 +1,5 @@
 ﻿using AdeNote.Infrastructure.Utilities;
+using AdeNote.Models;
 using AdeNote.Models.DTOs;
 
 namespace AdeNote.Infrastructure.Services.ChatService
@@ -10,5 +11,18 @@ namespace AdeNote.Infrastructure.Services.ChatService
         Task<ActionResult> CreateSubThread(CreateThreadDto message, string userId, string[] replyUserIds, string threadId);
 
         Task<ActionResult<TweetThreadDto>> GetThread(string threadId);
+
+        Task<ActionResult<List<TweetThreadDtos>>> GetThreads();
+
+        Task<ActionResult<SubThreadDto>> GetSubThread(string subThreadId, string parentId);
+
+        Task<ActionResult> UpdateThread(string threadId, UpdateThreadDto updateThread);
+
+        Task<ActionResult> UpdateSubThread(string subThreadId, string parentId, UpdateThreadDto updateThread);
+
+        Task<ActionResult> DeleteThread(string threadId);
+
+        Task<ActionResult> DeleteSubThread(string subThreadId, string parentId);
+
     }
 }
